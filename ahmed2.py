b2ea7eab7c3a144f22bc6b8954cac016cbe7c56e
Ahmed Ashraf <72763763+a5medashraf@users.noreply.github.com>
2023-10-04 15:23:00 +0300

    Create ahmed2.py

---
+++ ahmed2.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def add_numbers(num1, num2):
+    # Create NumPy arrays from the input numbers
+    arr1 = np.array(num1)
+    arr2 = np.array(num2)
+    
+    # Perform the addition
+    result = arr1 + arr2
+    
+    return result
+
+# Example usage:
+num1 = 5
+num2 = 3
+sum_result = add_numbers(num1, num2)
+print("Sum:", sum_result)
